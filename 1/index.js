64b64331323a9bbbaee6eb4221f9c4e518137872
bonmari <marina.marina2601@yandex.ru>
2024-01-27 13:20:43 +0300

    add ping service

--- 1/index.js
+++ 1/index.js
@@ -2,33 +2,52 @@ const readline = require('node:readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-readline.question(`Enter username \n`, (name) => {
-  readline.question(`Enter password \n`, (password) => {
-    connectDb(name, password);
-    readline.close();
-  });
-});
 
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 require('dotenv').config();
 
-const connectDb = async (name, password) => {
-  const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    // user: process.env.PG_USER,
-    // password: process.env.PG_PASSWORD,
-    user: name,
-    password: password,
-    database: process.env.PG_DATABASE,
-    ssl: false,
-  });
-  await client.connect();
-  const status = await client.query('SELECT $1::text as connected', [
-    'Connection to postgres successful!',
-  ]);
-  console.log(status.rows[0].connected);
-  const res = await client.query('select VERSION()::text as connected');
-  console.log(res.rows[0].connected);
-  await client.end();
+const config = {
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: false,
 };
+
+let dbConneted = false;
+
+const sqlQuery = 'select VERSION()::text as connected';
+console.log('config', config);
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: false,
+});
+const connect = () => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      return console.error('connection error', err);
+    }
+    client.query(sqlQuery, function (err, result) {
+      done();
+
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected');
+      dbConneted = true;
+    });
+  });
+};
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err) 
+})
+
+setInterval(() => {
+  connect()
+},  process.env.PING_INTERVAL * 1000);
